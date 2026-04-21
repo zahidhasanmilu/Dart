2dca84da89d122fba80bda437db50812192cdcf9
zahidhasanmilu <minions.milu889@gmail.com>
2026-04-22 02:59:03 +0600

    if/else

---
+++ if_else.dart
@@ -0,0 +1,40 @@
+/*
+// Result Shit Programms:
+void main(){
+  int mark = 30;
+
+  if (mark > 100 || mark <0){
+    print("Invalid Marks $mark");
+  } 
+  else if (mark>=80 && mark <= 100 ){
+    print("Great!");
+    print("Your Mark: $mark");
+    print("A+");
+  }
+  else if (mark>=70 && mark <= 79 ){
+    print("Great!");
+    print("Your Mark: $mark");
+    print("A");
+  }
+  else if (mark>=60 && mark <= 69 ){
+    print("Great!");
+    print("Your Mark: $mark");
+    print("B");
+  }
+  else if (mark>=50 && mark <= 59 ){
+    print("Great!");
+    print("Your Mark: $mark");
+    print("C");
+  }
+  else if (mark>=33 && mark <= 49 ){
+    print("Not Good!");
+    print("Your Mark: $mark");
+    print("D");
+  }
+  else{
+    print("Your Mark $mark");
+    print("You Failed.");
+  }
+}
+
+*/
