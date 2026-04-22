1499487145ee3b96d46d1603a03a60383bddd444
zahidhasanmilu <minions.milu889@gmail.com>
2026-04-22 07:12:16 +0600

    if/else

---
+++ loop.dart
@@ -0,0 +1,39 @@
+// int i = 0 → শুরু
+// i < 5 → কতক্ষণ চলবে
+// i++ → প্রতিবার ১ বাড়াও
+
+/*
+// jodi ami jani kotokkhon cholbe  tahole for loop
+void main(){
+  for( int i=0; i<10; i++){
+  print(i);
+}
+}
+
+// jodi na jani for loop kotokkhon cholbe, like totokkhon conndition true
+// hoyni totokkhon cholbe .
+
+
+//-----------------multiplication table generator
+import 'dart:io';
+
+void main(){
+  stdout.write("Enter the number for multiplication: ");
+  String? input = stdin.readLineSync();
+  int? num = int.tryParse(input?? "");
+
+  if (num==null){
+    print("Invalid Number");
+    return;
+  }
+ for (
+ int i=1;
+     i<=10;
+     i++
+ ){
+   print("$num x $i = ${num * i}");
+ }
+}
+
+*/
+
